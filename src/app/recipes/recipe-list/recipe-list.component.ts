25b7934802cb5f175abddd3fb0dc3aa0a4644048
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-04-02 12:29:07 +0200

    edit repicpe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Una ricetta test',
+      'test',
+      'https://www.misya.info/wp-content/uploads/2015/06/Pancakes-alla-nutella3.jpg'
+    ),
+  ];
 }
