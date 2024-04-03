614b84a0488a809dcb93fb38de6bf7757ff430b5
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-04-03 11:23:20 +0200

    visualizzazione dinamica delle ricette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { features } from 'node:process';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-recipes-book';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Una ricetta test',
       'test',
       'https://www.misya.info/wp-content/uploads/2015/06/Pancakes-alla-nutella3.jpg'
     ),
+    new Recipe(
+      'Un test diverso',
+      'BLABLABLABLA',
+      'https://www.misya.info/wp-content/uploads/2015/06/Pancakes-alla-nutella3.jpg'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
