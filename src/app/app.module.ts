2c255c13b8f31b2e8cbba2cab1465bba93270329
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-04-03 16:29:41 +0200

    custom directive per il dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FormsModule } from '@angular/forms';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [provideClientHydration()],
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,39 @@
+import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  private dropdownMenu: HTMLElement;
+
+  @HostListener('click') toggleOpen() {
+    this.dropdownMenu = this.elementRef.nativeElement.nextElementSibling;
+    this.toggleDropdown();
+  }
+
+  @HostListener('document:click', ['$event.target']) onDocumentClick(
+    target: HTMLElement
+  ) {
+    if (!this.elementRef.nativeElement.contains(target)) {
+      this.closeDropdown();
+    }
+  }
+
+  private toggleDropdown() {
+    if (this.dropdownMenu.classList.contains('show')) {
+      this.closeDropdown();
+    } else {
+      this.openDropdown();
+    }
+  }
+
+  private openDropdown() {
+    this.renderer.addClass(this.dropdownMenu, 'show');
+  }
+
+  private closeDropdown() {
+    this.renderer.removeClass(this.dropdownMenu, 'show');
+  }
+}
